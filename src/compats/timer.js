3609a0a368f7b92935dfc53d7bf894892570e9d4
simongfxu <xugaofan@gmail.com>
2015-10-23 14:43:17 +0800

    better timer

--- src/compats/timer.js
+++ src/compats/timer.js
@@ -25,30 +25,43 @@ if (engine.isEgret) {
 export {setTimeout, clearTimeout}
 
 /**
- * 来源于https://gist.github.com/simongfxu/ca6920a4658b3a4babd6
- * 现已改造为不要求轮询周期精度的模式
+ * 参考https://gist.github.com/simongfxu/ca6920a4658b3a4babd6
+ * 已改造为不要求轮询周期精度的模式
+ * 使用方式
+ * var timer = new Timer(func, 2000)
+ * 2000ms后自动开始执行，符合JS定时器默认逻辑
+ * timer.run()
+ * timer.stop()
+ * timer.reset(1000)
  */
 export default function Timer(fn, duration) {
-  this.status = 'constructed'
-
   /**
    * running期间多次调用会执行多次
    * 下个执行点为轮询执行完毕的duration之后
    */
-  this.run = function(){
+  this.duration = duration
+  this.status = 'running'
+  this.timer = setTimeout(() => this.run(), this.duration)
+
+  // 立即执行一次函数
+  this.run = () => {
     // 清除上次的定时器
     clearTimeout(this.timer)
 
-    this.status = 'running'
-
     attemp(fn)
 
-    var self = this
-    this.timer = setTimeout(() => self.run(), duration)
+    this.timer = setTimeout(() => this.run(), this.duration)
   }
 
-  this.stop = function(){
+  this.stop = () => {
     this.status = 'stopped'
     clearTimeout(this.timer)
+  }
+
+  // reset之后也会立即执行一次
+  this.reset = (num) => {
+    this.stop()
+    this.duration = num
+    this.run()
   }
 }
