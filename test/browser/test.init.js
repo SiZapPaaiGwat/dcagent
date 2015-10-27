7572c7fdc8db797b2b71fa386352d41b08b2897a
simongfxu <xugaofan@gmail.com>
2015-10-27 15:19:35 +0800

    fix jshintrc

--- test/browser/test.init.js
+++ test/browser/test.init.js
@@ -1,6 +1,10 @@
+/*globals describe, it, DCAgent*/
+
 describe('DCAgent.init', function() {
   it('should init with an appId', function() {
     DCAgent.init()
+    DCAgent.isReady().should.be.false()
+    DCAgent.init({appId: ''})
     DCAgent.isReady().should.be.false()
     DCAgent.init({appId: 'abc'})
     DCAgent.isReady().should.be.true()
