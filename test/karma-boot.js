f4d9576d6436e27c255cc67849f877aa27004bd6
simongfxu <xugaofan@gmail.com>
2015-11-01 22:23:22 +0800

    add karma test code

---
+++ test/karma-boot.js
@@ -0,0 +1,24 @@
+var DCAGENT_DEBUG_OPEN = true
+var ASAP_TIMEOUT = 2000
+
+// 每个Spec开始的时候重新加载SDK避免被上次的结果影响
+function loadDCAgent(done) {
+  var script = document.createElement('script')
+  script.src = 'base/dist/dcagent.v2.src.js?v=' + Date.now()
+  script.setAttribute('id', 'dcagent')
+  script.onload = done
+  document.body.appendChild(script)
+}
+
+// 每个Spec结束的时候销毁SDK，避免遗漏的Timer影响数据
+function destroyDCAgent(done) {
+  var script = document.querySelector('#dcagent')
+  document.body.removeChild(script)
+  DCAgent.destroy()
+  DCAgent = null
+  done()
+}
+
+jasmine.clock().install()
+// 这里也必须mockDate，不然有些Spec会失败
+jasmine.clock().mockDate(new Date(2015, 11, 1))
