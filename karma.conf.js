14ccdb23607c8460e9b9ec3c949bb35b22659d9f
simongfxu <xugaofan@gmail.com>
2015-12-01 19:06:06 +0800

    remove karma-coverage, use istabul directly in node to get coverage

--- karma.conf.js
+++ karma.conf.js
@@ -36,14 +36,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-    coverageReporter: {
-      reporters: [
-        { type: 'lcov', dir: './coverage', subdir: '.' },
-        { type: 'text-summary', dir: './coverage', subdir: '.' }
-      ]
-    },
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
