e3d46b80aacf89c42ec955b4c6679fcee4edba50
simongfxu <xugaofan@gmail.com>
2015-12-01 17:11:56 +0800

    fix typo and adjust code order

--- karma.conf.js
+++ karma.conf.js
@@ -36,8 +36,14 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      reporters: [
+        { type: 'lcov', dir: './coverage', subdir: '.' },
+        { type: 'text-summary', dir: './coverage', subdir: '.' }
+      ]
+    },
 
     // web server port
     port: 9876,
